2cd69774d99819f1922574aa91275ad774ecdb39
Jasson Rodriguez <jassonrm@icloud.com>
2020-10-11 16:00:37 -0600

    Add initial files

---
+++ showdata.m
@@ -0,0 +1,10 @@
+## Example of creating and showing the sensor raw data
+
+## Use for the experiments just 0,5% of the total available data.
+[X,z] = create(0.005);
+
+plot3(X(:,1),X(:,2),z(:),".");
+xlabel("x")
+ylabel("y")
+zlabel("z")
+
